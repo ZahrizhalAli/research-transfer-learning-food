b5db27f461519828f58e5b748bf00589a6191a0e
ZahrizhalAli <zzahrizhal.ali@gmail.com>
2022-06-18 19:15:13 +0800

    Finish experiment on the first base model.

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,29 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
-
-
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+def train_val_generators(DATA_DIR, SPLIT_SIZE, NUM_BATCHES):
     """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
 
+    :param DATA_DIR: Directory of the datasets was taken from (string)
+    :param SPLIT_SIZE: Split size of test set. (float)
+    :param NUM_BATCHES: Amount of batch size
+    :return:
+    """
+    datagen = ImageDataGenerator(rescale=1. / 255, validation_split=SPLIT_SIZE)
+    train_generator = datagen.flow_from_directory(directory=DATA_DIR,
+                                                  batch_size=NUM_BATCHES,
+                                                  class_mode='categorical',
+                                                  target_size=(300, 300),
+                                                  subset='training',
+                                                    shuffle = True,
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    )
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+    validation_generator = datagen.flow_from_directory(directory=DATA_DIR,
+                                                       subset='validation',
+                                                       batch_size=NUM_BATCHES,
+                                                       class_mode='categorical',
+                                                       shuffle=True,
+                                                       target_size=(300, 300))
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+    return train_generator, validation_generator
 
-    main()
+def print_something():
+    return "test"
